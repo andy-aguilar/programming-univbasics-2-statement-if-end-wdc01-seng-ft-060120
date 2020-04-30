d84aa9d4a89a121d0c718007e51904291630a39e
Andres Aguilar <aaguil3@gmail.com>
2020-04-30 21:33:56 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,5 @@
 # Write your solution here
 chance_of_rain = 0.9
-puts "Let's go outside!"
 if chance_of_rain <=0.25
   puts "Pack a sun shelter!"
 elsif (chance_of_rain >0.25 && chance_of_rain < 0.75)
