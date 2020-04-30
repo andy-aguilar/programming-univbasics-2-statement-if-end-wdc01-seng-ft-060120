c857c0306a9fc4d9751a0a90601f72eac1c76e9e
Andres Aguilar <aaguil3@gmail.com>
2020-04-30 21:51:49 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,10 @@
 # Write your solution here
 current_time = Time.now
+current_time = current_time.to_i
 
-if current_time.to_i = even
-  puts "You started working on this earlier!"
+if current_time % 2 == 0
+  puts "Even!"
 else
-  puts "You learned to go back in time!"
+  puts "Odd!"
 end
   
