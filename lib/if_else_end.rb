e9bb628ed719c1ac6349c6ba2bd1898d92984d7f
Andres Aguilar <aaguil3@gmail.com>
2020-04-30 21:48:11 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,8 @@
 # Write your solution here
 current_time = Time.now
 
-puts current_time.to_i
+if current_time.to_i > 1588283152
+  puts "You started working on this earlier!"
+else
+  puts "You learned to go back in time!"
+  
